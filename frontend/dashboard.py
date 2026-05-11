f597d62e7a3f41eb9a90bae20a1f39199751e249
Adnan Rafi Mohammed <adnanrafian@gmail.com>
2026-05-11 06:10:50 -0500

    Add Docker containerization

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 import time
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "http://backend:8000"
 
 st.set_page_config(
     page_title="Financial Market Sentiment Intelligence",
